81140cbb8dae43f9764588a5c9dc6ef7dfcfbc23
gs.8life <gs.8life@gmail.com>
2024-08-29 17:33:24 +0900

    조선욱 : Timer 형식 변경

--- Source/Unreal5_Portfolio/PartDevLevel/UI/HUD/Timer_UserWidget.cpp
+++ Source/Unreal5_Portfolio/PartDevLevel/UI/HUD/Timer_UserWidget.cpp
@@ -13,31 +13,16 @@ void UTimer_UserWidget::SetTimerCurStage(EGameStage _SetStage)
 
 void UTimer_UserWidget::SetTimer(int _Hour, int _Min, int _Second)
 {
-	if (0 == _Hour) {
-		SetTimer(_Min, _Second);
-		return;
-	}
-
 	NowTimeSpan = FTimespan(_Hour,_Min,_Second);
 	FText TextTemplate = FText::FromString("{_Hour}:{_Min}:{_Second}");
 
 	FFormatNamedArguments Arguments;
+	if (_Hour == 0) // 시간이 0이면 분,초만 표시되게 변경
+	{
 	Arguments.Add(TEXT("_Hour"), FText::AsNumber(_Hour));
+	}
 	Arguments.Add(TEXT("_Min"), FText::AsNumber(_Min));
 	Arguments.Add(TEXT("_Second"), FText::AsNumber(_Second));
-
-	Time = FText::Format(TextTemplate, Arguments);
-	TimeTextBlock->SetText(Time);
-}
-
-void UTimer_UserWidget::SetTimer(int _Min, int _Second)
-{
-	NowTimeSpan = FTimespan(0,_Min,_Second);
-	FText TextTemplate = FText::FromString("{_Min}:{_Second}");
-	;
-	FFormatNamedArguments Arguments;
-	Arguments.Add(TEXT("_Min"), UKismetTextLibrary::Conv_IntToText(NowTimeSpan.GetMinutes(), false, true, 2, 324));
-	Arguments.Add(TEXT("_Second"), UKismetTextLibrary::Conv_IntToText(NowTimeSpan.GetSeconds(), false, true, 2, 324));
 
 	Time = FText::Format(TextTemplate, Arguments);
 	TimeTextBlock->SetText(Time);
